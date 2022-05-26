ad7f57f61253ee7323c32038b8cdd9163ece4244
Sourav <sourav.iit.nstu@gmail.com>
2022-05-26 11:51:00 +0600

    get method

---
+++ GetMethod/Test.php
@@ -0,0 +1,9 @@
+<form action="./GetMethod.php" method="get">
+
+
+<input type="text" placeholder="user name" name="username"><br>
+<input type="password" placeholder="Password" name="password"><br>
+<input type="submit" value="submit" >
+
+
+</form>
