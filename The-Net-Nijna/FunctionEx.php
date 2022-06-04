bfc5bab33fe7315265ac0d30baf12bb9f749ed5d
Sourav <sourav.iit.nstu@gmail.com>
2022-06-04 11:55:15 +0600

    funciton example

---
+++ The-Net-Nijna/FunctionEx.php
@@ -0,0 +1,23 @@
+<?php
+/*
+function productPrice($product){
+
+   return "{$product['name']} costs dollar{$product['price']} to buy <br>";
+
+
+
+}
+
+$details = productPrice(['name'=>'gold star','price'=>20]);
+echo $details;
+*/
+
+function personName($name = "sourav"){
+    echo "Good morning $name";
+}
+
+personName(); // if there is no parameter then perosonName sourav will be showed
+
+
+
+?>
